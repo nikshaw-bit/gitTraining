b8e08b1471b0b450239f7a2c69488857959ec7b7
Nikhil <shawnikhilkumar83@gmail.com>
2023-05-10 00:42:18 +0530

    Second Commit

---
+++ view-product.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductServiceService } from '../product-service.service';
+
+@Component({
+  selector: 'app-view-product',
+  templateUrl: './view-product.component.html',
+  styleUrls: ['./view-product.component.css']
+})
+export class ViewProductComponent implements OnInit{
+  
+  mydate=String(new Date());
+  isExpired:boolean=false;
+  prod:Product[];
+  isDeleted:boolean=false;
+  constructor(private http:HttpClient,private service:ProductServiceService){}
+  ngOnInit(): void {
+    this.service.viewProduct()
+    .subscribe(data=>{
+      this.prod=data;
+    })
+  }
+  // delete(e){
+  //   let x=e.target.value;
+  //   // let id=this.activated.snapshot.params['id'].value;
+  //   this.service.deleteProduct(x)
+  //   .subscribe(data=>{
+  //     this.isDeleted=true;
+  //   });
+  // }
+  
+}
